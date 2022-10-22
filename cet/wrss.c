bf04dfd2eba17668a081d090c9764ac75fd7979f
Yi Sun <yi.sun@intel.com>
2022-10-22 14:25:08 +0800

    Reword License Identifier and Copyright
    Update License-Identifier with GPL-2.0-only because GPL-2.0 will be deprecated.
    And update the copyright for each file.
    
    Signed-off-by: Yi Sun <yi.sun@intel.com>

--- cet/wrss.c
+++ cet/wrss.c
@@ -1,4 +1,6 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright (c) 2022 Intel Corporation.
+
 /*
  * wrss.c: enable writable shadow stack and write value into shadow stack.
  *
