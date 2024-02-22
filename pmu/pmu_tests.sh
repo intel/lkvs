1cb316d90fd766337caf2c40def91364e2c33d44
Ammy Yi <ammy.yi@intel.com>
2024-02-22 15:27:39 +0800

    Add 5 new PMU cases for PMU
    Signed-off-by: Ammy Yi <ammy.yi@intel.com>

--- pmu/pmu_tests.sh
+++ pmu/pmu_tests.sh
@@ -74,6 +74,60 @@ basic_test() {
   should_fail "dmesg | grep 'generic architected perfmon'"
 }
 
+lbr_events_cpuid_test() {
+  #CPUID leaf 0x1c  ECX (19:16) must be all 1 for SRF.
+  for((i=16;i<=19;i++)); do
+    do_cmd "cpuid_check 1c 0 0 0 c $i"
+  done
+}
+
+lbr_events_s_test() {
+  perfdata="perf.data"
+  logfile="temp.txt"
+  do_cmd "perf record -o $perfdata -e "{branch-instructions,branch-misses}:S" -j any,counter sleep 1 >& $logfile"
+  sample_count=$(grep "sample" $logfile| awk '{print $10}' | tr -cd "0-9")
+  [[ $sample_count -eq 0 ]] && die "samples = 0!"
+  val=$(perf report -D -i $perfdata | grep -c "branch stack counters")
+  [[ $val -eq 0 ]] && die "branch stack counters val = 0!"
+  lbr_vals=$(perf report -D -i $perfdata | grep "branch stack counters" | awk '{print $5}')
+  for lbr_val in $lbr_vals; do
+    temp=$(echo "$lbr_val" | cut -d ":" -f 2)
+    test_print_trc "counts=$temp, lbr_val=$lbr_val!"
+    [[ $temp -eq 0 ]] && die "branch stack counters = 0!"
+  done
+}
+
+lbr_events_all_test() {
+  perfdata="perf.data"
+  logfile="temp.txt"
+  do_cmd "perf record -o $perfdata -e "{cpu/branch-instructions,branch_type=any/, cpu/branch-misses,branch_type=counter/}" sleep 1 >& $logfile"
+  sample_count=$(grep "sample" $logfile| awk '{print $10}' | tr -cd "0-9")
+  [[ $sample_count -eq 0 ]] && die "samples = 0!"
+  val=$(perf report -D -i $perfdata | grep -c "branch stack counters")
+  [[ $val -eq 0 ]] && die "branch stack counters val = 0!"
+  lbr_vals=$(perf report -D -i $perfdata | grep "branch stack counters" | awk '{print $5}')
+  for lbr_val in $lbr_vals; do
+    temp=$(echo "$lbr_val" | cut -d ":" -f 2)
+    test_print_trc "counts=$temp, lbr_val=$lbr_val!"
+    [[ $temp -eq 0 ]] && die "branch stack counters = 0!"
+  done 
+}
+
+timed_pebs_msr_test() {
+  #MSR_IA32_PERF_CAPABILITIES(0x345) bit 17 for Timed PEBs
+  bit_17=$(rdmsr 0x345 -f 17:17)
+  test_print_trc "MSR IA32_PERF_CAPABILITIES(0x345) bit 17 is: $bit_17"
+  [[ $bit_17 -eq 1 ]] || die "Timed PEBS msr bit is not set!"
+}
+
+uncore_events_test() {
+  uncore_events=$(perf list | grep uncore | grep PMU | awk '{print $1}')    
+  for uncore_event in $uncore_events; do
+    test_print_trc "uncore_event=$uncore_event"
+    do_cmd "perf stat -e $uncore_event sleep 1"
+  done
+}
+
 pmu_test() {
   echo "$WATCHDOG" > /proc/sys/kernel/nmi_watchdog
   value=$(cat /proc/sys/kernel/nmi_watchdog)
@@ -87,6 +141,21 @@ pmu_test() {
       ;;
     uncore)
       do_cmd "ls /sys/devices/ | grep uncore"
+      ;;
+    lbr_events_cpuid)
+      lbr_events_cpuid_test
+      ;;
+    lbr_events_s)
+      lbr_events_s_test
+      ;;
+    lbr_events_all)
+      lbr_events_all_test
+      ;;
+    timed_pebs_msr)
+      timed_pebs_msr_test
+      ;;
+    uncore_events)
+      uncore_events_test
       ;;
     esac
   return 0
