e280f92051e4f9c902280b3effdcd8c6089325cd
Yi Sun <yi.sun@intel.com>
2024-06-27 13:38:18 +0800

    Avocadorun: Refine the arguments.
    Signed-off-by: Yi Sun <yi.sun@intel.com>

--- BM/Avocado-run.py
+++ BM/Avocado-run.py
@@ -9,36 +9,31 @@ from avocado.core.suite import TestSuite
 
 # Parse command line arguments.
 parser = argparse.ArgumentParser(description='Run tests from a list in a file(tests/tests-client/tests-server).')
-parser.add_argument('file_path', help='Path to the file containing the list of tests to run')
+parser.add_argument('-f', '--feature', help='Directory of a feature to run the tests')
+parser.add_argument('-t', '--tests', help='Path to a test file containing the list of tests to run')
 args = parser.parse_args()
-script_dir = os.path.dirname(args.file_path)
 
 # Check the dependency, if the exit value is not 0, then terminate the test.
-bash_script = f"./runtests -d {args.file_path}"
-result = subprocess.run(bash_script, shell=True)
-if result.returncode != 0:
-    # Terminate the test.
-    sys.exit(1) 
-
+# TODO
 
 # Read the tests file and create Runnable objects.
-def create_runnables_from_file(file_path, script_dir):
+def create_runnables_from_file(ftests, feature_dir):
     tests = []
-    with open(file_path, 'r') as file:
+    with open(ftests, 'r') as file:
         for line in file:
             # Handle empty lines and comments.
             if not line.strip() or line.startswith('#'):
                 continue
             # Split command line parameters.
             parts = line.strip().split()
-            script_path = os.path.join(script_dir, parts[0])
+            script_path = os.path.join(feature_dir, parts[0])
             # Create a Runnable object.
             runnable = Runnable("exec-test", script_path, *parts[1:])
             tests.append(runnable)
     return tests
 
 def main():
-    tests = create_runnables_from_file(args.file_path, script_dir)
+    tests = create_runnables_from_file(args.tests, args.feature)
 
     # Create a test suite and add tests.
     suite = TestSuite(name="lkvs-test", tests=tests)
