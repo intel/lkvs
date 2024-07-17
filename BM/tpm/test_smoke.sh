41cebf810a66a0d9d6d82e84f0a379077b8ac8cc
Pengfei Xu <pengfei.xu@intel.com>
2024-07-17 17:18:05 +0800

    BM/tpm: add 15 tpm test cases
    It includes tpm smoke, content, async 15 test cases from kself-test
    and marked in the code file header.
    In order to be compatible and executed under LKVS, some compatibility
    adjustments like PYTHONPATH have been made.
    
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

---
+++ BM/tpm/test_smoke.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# SPDX-License-Identifier: (GPL-2.0 OR BSD-3-Clause)
+
+# Kselftest framework requirement - SKIP code is 4.
+ksft_skip=4
+
+[ -e /dev/tpm0 ] || exit $ksft_skip
+
+# shellcheck disable=SC3028,SC3054
+TPM_ROOT=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+export PYTHONPATH="$TPM_ROOT"
+python3 -m unittest -v tpm2_tests.SmokeTest
