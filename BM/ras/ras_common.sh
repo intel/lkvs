10fbc5df8eb2b7071883193bd243722242b0a828
Lai, Yi1 <yi1.lai@intel.com>
2024-08-28 17:48:01 +0800

    ras: add LMCE feature test cases
    Add 18 test cases for LMCE feature.
    
    Signed-off-by: Lai, Yi1 <yi1.lai@intel.com>

--- BM/ras/ras_common.sh
+++ BM/ras/ras_common.sh
@@ -7,6 +7,10 @@
 cd "$(dirname "$0")" 2>/dev/null || exit 1
 source ../.env
 
+MCELOG_LOGFILE=/var/log/mcelog
+MCA_BANK_NUM=$((0x$(rdmsr 0x179) & 0xFF)) # 0x179 is IA32_MCG_CAP
+IA32_MCi_CTL2=$(rdmsr 0x280) # 0x280 is IA32_MCi_CTL2
+
 # check whether one package is installed, if not, install the package
 # Usage: pkg_check_install pkg_name_1 pkg_name_2 pkg_name_x
 pkg_check_install() {
@@ -19,5 +23,49 @@ pkg_check_install() {
     else
       test_print_trc "${pkg_name} is already installed"
     fi
+  done
+}
+
+# check mcelog service is properly configured and running
+mcelog_config() {
+  local daemon=0
+  local logfile=0
+
+  pgrep -x mcelog >/dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    test_print_trc "mcelog service is not running, start the service"
+    /usr/sbin/mcelog --ignorenodev --daemon --logfile=${MCELOG_LOGFILE}
+    exit 0
+  fi
+  for i in $(pgrep -a -x mcelog); do
+    if [[ $i == *"--daemon"* ]]; then
+      daemon=1
+    elif [[ $i == *"--logfile=${MCELOG_LOGFILE}"* ]]; then
+      logfile=1
+    fi
+  done
+  if [ "$daemon" -eq 0 ] || [ "$logfile" -eq 0 ]; then
+    test_print_trc "mcelog service is not properly configured, reload the service."
+    kill -9 $(pgrep -x mcelog)
+    /usr/sbin/mcelog --ignorenodev --daemon --logfile=${MCELOG_LOGFILE}
+  fi
+}
+
+# disable MCE CMCI
+disable_cmci() {
+  for (( bank=0; bank<MCA_BANK_NUM; bank++ )); do
+    msr_address=$((0x280 + bank))
+		current_value=$(rdmsr -p 0 $msr_address)
+    current_value_dec=$((0x$current_value))
+    new_value_dec=$((current_value_dec & ~(1 << 30)))
+    wrmsr -a $msr_address $new_value_dec # write to all processors
+  done
+}
+
+# enable MCE CMCI
+enable_cmci() {
+  for ((bank=0; bank<MCA_BANK_NUM; bank++)); do
+    msr_address=$((0x280 + bank))
+    wrmsr -a $msr_address "0x${IA32_MCi_CTL2}"
   done
 }
