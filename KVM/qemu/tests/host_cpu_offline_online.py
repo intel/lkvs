cfc6837cab43aa98719a8e8ffbcca99a3e70f248
Xudong Hao <xudong.hao@intel.com>
2024-12-11 16:12:28 +0800

    KVM: Add CPU offline/online cases with TD/VM booting
    Signed-off-by: Xudong Hao <xudong.hao@intel.com>

---
+++ KVM/qemu/tests/host_cpu_offline_online.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (c) 2024 Intel Corporation
+
+# Author: Xudong Hao <xudong.hao@intel.com>
+#
+# History: Dec. 2024 - Xudong Hao - creation
+
+import random
+from avocado.utils import cpu
+
+from virttest.cpu import check_if_vm_vcpu_match
+
+
+def run(test, params, env):
+    """
+    pCPU offline/online test:
+    1) Launch a guest with many CPU.
+    2) Offline 3 random online pCPU.
+    3) Check guest's status
+    4) Online the offlined pCPU again.
+
+    :param test: QEMU test object
+    :param params: Dictionary with the test parameters
+    :param env: Dictionary with test environment.
+    """
+    vm = env.get_vm(params["main_vm"])
+    vcpus = params["smp"]
+    vm.verify_alive()
+    timeout = params.get_numeric("login_timeout", 240)
+    session = vm.wait_for_login(timeout=timeout)
+
+    host_cpu_list = cpu.online_list()
+    processor_list = random.sample(host_cpu_list, 3)
+    for processor in processor_list:
+        cpu.offline(processor)
+    test.log.info("CPUs {} have been offline.".format(processor_list))
+    if not check_if_vm_vcpu_match(vcpus, vm):
+        test.fail("vCPU quantity on guest mismatch after offline")
+
+    for processor in processor_list:
+        cpu.online(processor)
+    test.log.info("CPUs {} have been online.".format(processor_list))
+    session.close()
