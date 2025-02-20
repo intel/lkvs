901aaf81a897784183e26bcc26fcc1f4cc11f990
Xudong Hao <xudong.hao@intel.com>
2025-02-20 09:16:19 +0800

    KVM: Enable cpuid direct configuration
    Example:
    - new_cpuid_check
    	cpuid = "7 0 0 0 a 5"
    
    Signed-off-by: Xudong Hao <xudong.hao@intel.com>

--- KVM/qemu/tests/x86_cpuid.py
+++ KVM/qemu/tests/x86_cpuid.py
@@ -54,18 +54,14 @@ def prepare_cpuid(test, params, src_dir, vm=None, session=None):
     return os.path.join(test_dir, exec_file)
 
 
-def check_cpuid(params, feature, exec_bin, session=None):
+def check_cpuid(cpuid_arg, exec_bin, session=None):
     """
     Run cpuid test in host or guest
-    :param params: Dictionary with the test parameters
-    :param feature: The feature to be checked
+    :param cpuid_arg: The feature to be checked
     :param exec_bin: The execuable bianry tool with absolute path
     :param session: Guest session
     """
-    args = feature_list[feature]["cpuid"]
-    args_str = ' '.join(args)
-
-    check_cmd = '%s %s' % (exec_bin, args_str)
+    check_cmd = '%s %s' % (exec_bin, cpuid_arg)
     func = process.getstatusoutput
     if session:
         func = session.cmd_status_output
@@ -86,21 +82,54 @@ def run(test, params, env):
     :param params: Dictionary with the test parameters
     :param env: Dictionary with test environment.
     """
+    def host_feature_cpuid():
+        for feature_name in feature_names.split():
+            args = feature_list[feature_name]["cpuid"]
+            cpuid_arg = ' '.join(args)
+            host_status = check_cpuid(cpuid_arg, host_exec_bin)  # pylint: disable=E0606
+            if host_status:
+                process.system("rm %s -f" % host_exec_bin, shell=True, ignore_status=True)
+                test.cancel("Platform doesn't support %s" % feature_name)
+        test.log.info("Host cpuid check %s pass.", feature_names)
+
+    def host_check_cpuid():
+        cpuid_args = params["cpuid"]
+        for cpuid_arg in cpuid_args.split(","):
+            host_status = check_cpuid(cpuid_arg, host_exec_bin)   # pylint: disable=E0606
+            if host_status:
+                process.system("rm %s -f" % host_exec_bin, shell=True, ignore_status=True)
+                test.cancel("Platform doesn't support %s" % cpuid_arg)
+        test.log.info("Host cpuid check %s pass.", cpuid_args)
+
+    def guest_feature_cpuid():
+        for feature_name in feature_names.split():
+            args = feature_list[feature_name]["cpuid"]
+            cpuid_arg = ' '.join(args)
+            guest_status = check_cpuid(cpuid_arg, vm_exec_bin, session)
+            if guest_status:
+                test.fail("%s cpuid check fail in guest." % feature_name)
+        test.log.info("Guest cpuid check %s pass.", feature_names)
+
+    def guest_check_cpuid():
+        cpuid_args = params["cpuid"]
+        for cpuid_arg in cpuid_args.split(","):
+            guest_status = check_cpuid(cpuid_arg, vm_exec_bin, session)
+            if guest_status:
+                test.fail("%s cpuid check fail in guest." % cpuid_arg)
+        test.log.info("Guest cpuid check %s pass.", cpuid_args)
+
     test_dir = params["test_dir"]
+    feature_names = params.get("features")
     bm_dir = get_baremetal_dir(params)
     src_dir = "%s/tools/cpuid_check" % bm_dir
 
     check_host_cpuid = params.get_boolean("check_host_cpuid")
     if check_host_cpuid:
         host_exec_bin = prepare_cpuid(test, params, src_dir)
-
-        feature_names = params["features"]
-        for feature_name in feature_names.split():
-            host_status = check_cpuid(params, feature_name, host_exec_bin)
-            if host_status:
-                process.system("rm %s -f" % host_exec_bin, shell=True, ignore_status=True)
-                test.cancel("Platform doesn't support %s" % feature_name)
-        test.log.info("Host cpuid check %s pass.", feature_names)
+        if feature_names:
+            host_feature_cpuid()
+        else:
+            host_check_cpuid()
 
     try:
         params["start_vm"] = "yes"
@@ -110,11 +139,11 @@ def run(test, params, env):
         session = vm.wait_for_login()
 
         vm_exec_bin = prepare_cpuid(test, params, src_dir, vm, session)
-        for feature_name in feature_names.split():
-            guest_status = check_cpuid(params, feature_name, vm_exec_bin, session)
-            if guest_status:
-                test.fail("%s cpuid check fail in guest." % feature_name)
-        test.log.info("Guest cpuid check %s pass.", feature_names)
+        if feature_names:
+            guest_feature_cpuid()
+        else:
+            guest_check_cpuid()
+
     finally:
         process.system("rm %s -f" % host_exec_bin, shell=True, ignore_status=True)
         session.cmd("rm %s/cpuid* -rf" % test_dir, ignore_all_errors=True)
