d8e77a542205fd366455164ccee062f96542eb73
Farrah Chen <farrah.chen@intel.com>
2025-04-01 02:42:31 -0400

    tdx-compliance: Add TD guest specified config for #VE Reduction
    Setting TDCS.TD_CLS and TDCS.FEATURE_PARAVIRT_CTLS field, free to
    any combination of these 2 fields for validation purpose.
    
    This part is porting from tdx kvm unit test
    
    Signed-off-by: Farrah Chen <farrah.chen@intel.com>

--- BM/tdx-compliance/tdx-compliance-main.c
+++ BM/tdx-compliance/tdx-compliance-main.c
@@ -38,6 +38,8 @@ char version_name[32];
 char *buf_ret;
 bool kretprobe_switch;
 static struct dentry *f_tdx_tests, *d_tdx;
+static u64 tdcs_td_ctl;
+static u64 tdcs_feature_pv_ctl;
 LIST_HEAD(cpuid_list);
 
 #define SIZE_BUF		(PAGE_SIZE << 3)
@@ -147,7 +149,12 @@ static int run_cpuid(struct test_cpuid *t)
 {
 	t->regs.eax.val = t->leaf;
 	t->regs.ecx.val = t->subleaf;
+	tdcs_td_ctl = t->tdcs_td_ctl;
+	tdcs_feature_pv_ctl = t->tdcs_feature_pv_ctl;
+	setup_tdcs_ctl();
 	__cpuid(&t->regs.eax.val, &t->regs.ebx.val, &t->regs.ecx.val, &t->regs.edx.val);
+	int cpu_id = smp_processor_id();
+	printk(KERN_INFO "From CPU core %d\n", cpu_id);
 
 	return 0;
 }
@@ -614,6 +621,104 @@ u64 tdcall(u64 fn, struct tdx_module_args *args)
 	if (r)
 		panic("TDCALL %lld failed (Buggy TDX module!)\n", fn);
 	return r;
+}
+
+u64 tdg_vm_read(u64 field, u64 *val)
+{
+   u64 ret;
+   struct tdx_module_args arg = {
+       .rcx = 0,
+       .rdx = field,
+   };
+
+   ret =  __tdcall_ret(TDG_VM_RD, &arg);
+   if (!ret)
+       *val = arg.r8;
+
+   return ret;
+}
+
+u64 tdg_vm_write(u64 field, u64 val, u64 mask)
+{
+   struct tdx_module_args arg = {
+       .rcx = 0,
+       .rdx = field,
+       .r8 = val,
+       .r9 = mask,
+   };
+
+   return tdcall(TDG_VM_WR, &arg);
+}
+
+static void setup_tdcs_ctl(void)
+{
+   struct tdx_module_args arg;
+   u64 r;
+   u64 v;
+   u64 mask;
+   bool ve_reduce;
+   bool vcpu_toplgy;
+
+   arg.rdx = TDX_GLOBAL_FIELD_FEATURES0;
+   r = tdcall(TDG_SYS_RD, &arg);
+   if (r) {
+       printk("WARN: Failed to get TDX FEATURES0, err:0x%llx\n", r);
+       return;
+   }
+
+   printk("TDX FEATURES0:0x%llx\n", arg.r8);
+
+   r = tdg_vm_read(TDX_TDCS_FIELD_TD_CTL, &v);
+   if (!r)
+       printk("TDX_TDCS_FILED_TD_CTL before set:0x%llx\n", v);
+   else
+       printk("Failed to read TDX_TDCS_FILED_TD_CTL\n");
+
+   r = tdg_vm_read(TDX_TDCS_FIELD_FEATURE_PV_CTL, &v);
+   if (!r)
+       printk("TDX_TDCS_FIELD_FEATURE_PV_CTL before set:0x%llx\n", v);
+   else
+       printk("Failed to read TDX_TDCS_FIELD_FEATURE_PV_CTL\n");
+
+   ve_reduce = arg.r8 & MD_FIELD_ID_FEATURES0_VE_REDUCE;
+   vcpu_toplgy = arg.r8 & MD_FIELD_ID_FEATURES0_VCPU_TPLGY;
+   if ((ve_reduce || vcpu_toplgy) && tdcs_td_ctl != 0xbad) {
+       mask = tdcs_td_ctl ? tdcs_td_ctl : 0x800000000000000eULL;
+       r = tdg_vm_write(TDX_TDCS_FIELD_TD_CTL, tdcs_td_ctl, mask);
+       if (!r)
+           printk("TDX_TDCS_FILED_TD_CTL set to 0x%llx\n",
+           tdcs_td_ctl);
+       else
+           printk("TDX_TDCS_FILED_TD_CTL set to 0x%llx failure, err:0x%llx\n",
+           tdcs_td_ctl, r);
+   } else
+       printk("TDX_TDCS_FILED_TD_CTL is not supported or not set (0x%llx)\n", tdcs_td_ctl);
+
+   if (ve_reduce && tdcs_td_ctl != 0xbad) {
+       mask = tdcs_feature_pv_ctl ? tdcs_feature_pv_ctl : -1ULL;
+       r = tdg_vm_write(TDX_TDCS_FIELD_FEATURE_PV_CTL,
+                        tdcs_feature_pv_ctl, mask);
+       if (!r)
+           printk("TDX_TDCS_FIELD_FEATURE_PV_CTL set to 0x%llx\n",
+                  tdcs_feature_pv_ctl);
+       else
+           printk("TDX_TDCS_FIELD_FEATURE_PV_CTL set to 0x%llx failure, err:0x%llx\n",
+                  tdcs_feature_pv_ctl, r);
+   } else
+       printk("TDX_TDCS_FIELD_FEATURE_PV_CTL is not supported or not set (tdcs_ctl=0x%llx)\n", tdcs_td_ctl);
+
+   r = tdg_vm_read(TDX_TDCS_FIELD_TD_CTL, &v);
+   if (!r)
+       printk("TDX_TDCS_FILED_TD_CTL after set:0x%llx\n", v);
+   else
+       printk("Failed to read TDX_TDCS_FILED_TD_CTL\n");
+
+   r = tdg_vm_read(TDX_TDCS_FIELD_FEATURE_PV_CTL, &v);
+   if (!r)
+       printk("TDX_TDCS_FIELD_FEATURE_PV_CTL after set:0x%llx\n", v);
+   else
+       printk("Failed to read TDX_TDCS_FIELD_FEATURE_PV_CTL\n");
+
 }
 
 static int __init tdx_tests_init(void)
